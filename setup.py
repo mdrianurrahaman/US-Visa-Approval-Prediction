9add1c0a401f0a2987f81699bdbcadad9bed3c6c
rian <mdrianurrahaman@gmail.com>
2024-02-23 00:15:13 +0530

    updated files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rian",
+    author_email="mdrianurrahaman@gmail.com",
+    packages=find_packages()
+)
