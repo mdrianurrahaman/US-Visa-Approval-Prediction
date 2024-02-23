ea89572fe7da5c6d2df322ce9ac418b6ebb39d5d
rian <mdrianurrahaman@gmail.com>
2024-02-23 22:02:58 +0530

    updated files

---
+++ demo.py
@@ -0,0 +1,8 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+try:
+    a = 1 / "10"
+except Exception as e:
+    raise USvisaException(e, sys) from e
